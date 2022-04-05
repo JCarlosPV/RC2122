b5c0382f3eb560617bd32399362422f8d5d838c7
JCarlosPV <102228408+JCarlosPV@users.noreply.github.com>
2022-04-05 09:51:13 +0200

    Update mas_veces.pl

--- mas_veces.pl
+++ mas_veces.pl
@@ -6,9 +6,25 @@
 		repite dicho elemento.
 */
 
-%mas_veces([X], X, 1).
-%mas_veces([Cab | Resto], , ):- 
+mas_veces([], _, 0).
+mas_veces([(X, N)], X, N).
+mas_veces(Lista, Elem, N):- msort(Lista, RO), comprime(RO, R), mayor(R, Elem, N).
 
+
+/*
+	mayor(+Lista, -Elem, -N)
+		es cierto si Elem unifica con 
+		el elemento que se repita 
+		mas veces en Lista y N unifica con el 
+		número de veces que se repite.
+*/
+
+mayor([], _, 0).
+mayor([(Cab, N) | Resto], Cab, N):- mayor(Resto, _, N2), N > N2.
+mayor([(_, N) | Resto], E, N2):- mayor(Resto, E, N2), N =< N2.
+
+
+%[(a, 2), (b, 1), (c, 3)]
 
 /*
 	comprime(+Lista, -R)
@@ -18,6 +34,14 @@
 */
 
 comprime([], [(_, 0)]).
-comprime([Cab 
+comprime([Elem], [(Elem, 1)]).
 
+comprime([Cab, Cab | Resto], [(Cab, N2) | R]):- 
+	comprime([Cab | Resto], [(Cab, N) | R]),
+	N2 is N + 1.
+
+comprime([Elem1, Elem2 | Resto],[(Elem1, 1) | R]):- 
+	Elem1 \= Elem2, 
+	comprime([Elem2 | Resto], R).
+	
 
